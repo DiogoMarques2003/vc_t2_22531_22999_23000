6f3a504a3393372127a0fcfafded98a6834f7b9d
Diogo Marques <diogomarques2003@protonmail.com>
2024-05-31 21:26:44 +0100

    salvar cores

--- main.cpp
+++ main.cpp
@@ -13,16 +13,16 @@ extern "C"
 
 // Defenir array com os valores do HSV
 HSV hsv_values[10] = {
-    // {0, 360, 0, 15, 20, 30, 15, 15, 30},    // Preto -> 0
-    {15, 25, 40, 56, 30, 50, 15, 15, 10},  // Castanho -> 1
-    // {340, 14, 60, 90, 67, 100, 1, 11}, // Vermelho -> 2
-    // {25, 26, 70, 75, 80, 85, -1, -1},  // Laranja -> 3
-    // {64, 67, 100, 102, 100, 102, -1, -1},  // Amarelo -> 4
-    // {100, 165, 30, 100, 30, 100, 1, 17}, // Verde -> 5
-    // {165, 255, 15, 100, 35, 100, 1, 5}, // Azul -> 6
-    // {270, 320, 30, 100, 30, 100, -1, -1}, // Roxo -> 7
-    // {75, 80, 8, 10, 17, 30, -1, -1},    // Cinza -> 8
-    // {0, 360, 0, 0, 100, 100, -1, -1}   // Branco -> 9
+    {0, 360, 0, 15, 20, 30, 15, 15, 30},    // Preto -> 0
+    {15, 25, 40, 56, 30, 50, 15, 15, 30},  // Castanho -> 1
+    {340, 14, 60, 90, 67, 100, 11, 11, 15}, // Vermelho -> 2
+    {25, 26, 70, 75, 80, 85, -1, -1, -1},  // Laranja -> 3
+    {64, 67, 100, 102, 100, 102, -1, -1, -1},  // Amarelo -> 4
+    {100, 165, 30, 100, 30, 100, 17, 17, 30}, // Verde -> 5
+    {165, 255, 15, 100, 35, 100, 5, 5, 10}, // Azul -> 6
+    {270, 320, 30, 100, 30, 100, -1, -1, -1}, // Roxo -> 7
+    {75, 80, 8, 10, 17, 30, -1, -1, -1},    // Cinza -> 8
+    {0, 360, 0, 0, 100, 100, -1, -1, -1}   // Branco -> 9
 };
 
 int hsvColorsInt = sizeof(hsv_values) / sizeof(HSV);
@@ -127,7 +127,7 @@ int main(void)
 		memset(imageSegmented->data, 0, video.width * video.height * 1);
 
 		// // Se encontrou pixeis, segmentar pelas cores das riscas das resistências
-		// if (foundPixeis == 1) {
+		if (foundPixeis == 1) {
 			for (int i = 0; i < hsv_count; i++) {
 				// Se o hsv tiver erosão ou dilatação com -1 é porque não aparece no video
 				if (hsv_values[i].erosao == -1 || hsv_values[i].dilatacao == -1) {
@@ -148,14 +148,12 @@ int main(void)
 					}
 				}
 			}
-		// }
+		}
 
 		// Dilatar e erodir a imagem para remover o espaço em branco por causa das linhas a cor da resistência
-		//TODO: validar quais os melhores valores para isto -> 19, 19 é o que está a funcionar melhor para o opencv
-		// vc_binary_close(imageSegmented, imageClosed, 1, 11);
 		// Usada a função de dilatação do opencv por ser mais eficiente quando tem valores/imagens grandes
 		opencvSegmentation = cv::Mat(imageSegmented->height, imageSegmented->width, CV_8UC1, imageSegmented->data);
-		cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(15, 15));
+		cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(19, 19));
 		cv::morphologyEx(opencvSegmentation, opencvClose, cv::MORPH_CLOSE, element);
 		memcpy(imageClosed->data, opencvClose.data, video.width * video.height * 1);
 
@@ -164,31 +162,31 @@ int main(void)
 		OVC *blobs = vc_binary_blob_labelling(imageClosed, imageBlobs, &nblobs);
 
 		// Se encontrou blobs
-		if (blobs != NULL) {
-			// Obter informação dos blobs
-			vc_binary_blob_info(imageBlobs, blobs, nblobs);
+		// if (blobs != NULL) {
+		// 	// Obter informação dos blobs
+		// 	vc_binary_blob_info(imageBlobs, blobs, nblobs);
 
-			// Percorrer os blobs
-			for (int i = 0; i < nblobs; i++) {
-				// Se o blob estiver a menos de 1% do início e 10% do fim da imagem, ignorar para garantir que a resistência está toda na imagem
-				if (blobs[i].y < 0.08 * imageBlobs->height || blobs[i].y > 0.90 * imageBlobs->height) {
-					continue;
-				}
+		// 	// Percorrer os blobs
+		// 	for (int i = 0; i < nblobs; i++) {
+		// 		// Se o blob estiver a menos de 1% do início e 10% do fim da imagem, ignorar para garantir que a resistência está toda na imagem
+		// 		if (blobs[i].y < 0.08 * imageBlobs->height || blobs[i].y > 0.90 * imageBlobs->height) {
+		// 			continue;
+		// 		}
 				
-				// Se a área da fita for muito pequena avançar
-				if (blobs[i].area < 10) {
-					continue;
-				}
+		// 		// Se a área da fita for muito pequena avançar
+		// 		if (blobs[i].area < 10) {
+		// 			continue;
+		// 		}
 
-				cv::Point pt1(blobs[i].x, blobs[i].y);
-				cv::Point pt2(blobs[i].x + blobs[i].width, blobs[i].y + blobs[i].height);
-            	cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
+		// 		cv::Point pt1(blobs[i].x, blobs[i].y);
+		// 		cv::Point pt2(blobs[i].x + blobs[i].width, blobs[i].y + blobs[i].height);
+        //     	cv::rectangle(frame, pt1, pt2, cv::Scalar(0, 255, 0), 2);
 
-				// colocar area da risca no ecrã
-				str = i + " -- " + std::to_string(blobs[i].area);
-				cv::putText(frame, str, cv::Point(blobs[i].x, blobs[i].y - 20), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 255, 0), 2);
-			}
-		}
+		// 		// colocar area da risca no ecrã
+		// 		str = i + " -- " + std::to_string(blobs[i].area);
+		// 		cv::putText(frame, str, cv::Point(blobs[i].x, blobs[i].y - 20), cv::FONT_HERSHEY_SIMPLEX, 1.0, cv::Scalar(0, 255, 0), 2);
+		// 	}
+		// }
 
 		// cv::Mat imageToShow = cv::Mat(imageClosed->height, imageClosed->width, CV_8UC3);
         // for (int y = 0; y < imageClosed->height; y++) {
@@ -264,6 +262,7 @@ int main(void)
 
 		// 				// Percorrer os blobs
         //            		for (int k = 0; k < nblobsBlob; k++) {
+	    //				//TODO: Adicionar a verificação pela area do blobs da risca
 		// 					printf("Cor encontrada: %d\n", j);
         //                 	colors[countColors].color = j;
         //                 	colors[countColors].x = blobsBlob[k].xc;
